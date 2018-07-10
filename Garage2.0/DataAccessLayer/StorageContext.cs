98728ed0c58b047d354ccbbe8dd12ab790b2e367
nour aldeen <nour.aldeen11@outlook.com>
2018-07-10 13:17:38 +0200

    Add project files.

---
+++ Garage2.0/DataAccessLayer/StorageContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Garage2._0.DataAccessLayer
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext() : base("DefaultConnectionGarage")
+        {
+        }
+
+        public DbSet<Models.ParkedVehicle> parkedVehicles { get; set; }
+        
+    }
+}
